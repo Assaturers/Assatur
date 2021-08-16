6e305e3711d05fe0785874892448173ecb32f8e0
webmilio <therealwebmilio@gmail.com>
2021-08-16 15:03:26 -0400

    [PATCH] Created SLN.

---
+++ ModLoader/Patches/Magicka.sln.patch.c
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.31402.337
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Magicka", "Magicka.csproj", "{9E2745D1-59D8-4EF4-BCDC-9469B65C3203}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{9E2745D1-59D8-4EF4-BCDC-9469B65C3203}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{9E2745D1-59D8-4EF4-BCDC-9469B65C3203}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{9E2745D1-59D8-4EF4-BCDC-9469B65C3203}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{9E2745D1-59D8-4EF4-BCDC-9469B65C3203}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {B9E344AE-4D45-4BAB-9949-3FF891411585}
+	EndGlobalSection
+EndGlobal
