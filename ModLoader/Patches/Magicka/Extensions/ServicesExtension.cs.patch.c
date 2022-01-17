300124a8d30787a965e7d1c4885bdd2a480f4545
webmilio <webmilio@users.noreply.github.com>
2022-01-17 18:29:43 -0500

    Added ServicesExtension for type-generic service obtention.

---
+++ ModLoader/Patches/Magicka/Extensions/ServicesExtension.cs.patch.c
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Magicka.Extensions;
+
+public static class ServicesExtension
+{
+    public static T GetService<T>(this IServiceContainer services)
+    {
+        return (T) services.GetService(typeof(T));
+    }
+}
