faf040dc9fa1787f369bc7957339aa3d77cb40aa
webmilio <webmilio@users.noreply.github.com>
2022-01-17 17:58:04 -0500

    [#4] Started work on DLL loading.

---
+++ ModLoader/Patches/Magicka/ModLoader/ModLoader.cs.patch.c
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Webmilio.Commons.Assemblies;
+
+namespace Magicka.ModLoader;
+
+public class ModLoader
+{
+    private class ModAssembliesResolver : IAssemblyResolver
+    {
+        public IList<Assembly> Resolve()
+        {
+
+        }
+    }
+}
