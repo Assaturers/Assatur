964bbe56f2844a479c6d14bf3351afc77df7366c
webmilio <webmilio@users.noreply.github.com>
2022-01-22 23:52:44 -0500

    Added extensions for assemblies/enumerables.

---
+++ ModLoader/Patches/Magicka/Extensions/AssembliesExtension.cs.patch.c
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Magicka.Extensions;
+
+public static class AssembliesExtension
+{
+    public static List<Type> Subclasses<T>(this IEnumerable<Type> types) => FindTypes<T>(types, IsSubclassOf<T>);
+
+    public static List<Type> ConcreteSubclasses<T>(this IEnumerable<Type> types) => FindTypes<T>(types, IsConcreteSubclass<T>);
+
+    public static List<Type> FindTypes<T>(this IEnumerable<Type> types, Predicate<Type> predicate)
+    {
+        List<Type> t = new();
+
+        foreach (var type in types)
+            if (predicate(type))
+                t.Add(type);
+
+        return t;
+    }
+
+    private static bool IsSubclassOf<T>(Type type) => type.IsSubclassOf(typeof(T));
+
+    private static bool IsConcrete(Type type) => !type.IsAbstract && !type.IsInterface;
+
+    private static bool IsConcreteSubclass<T>(Type type) => IsConcrete(type) && IsSubclassOf<T>(type);
+}
---
+++ ModLoader/Patches/Magicka/Extensions/EnumerablesExtension.cs.patch.c
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Magicka.Extensions;
+
+public static class EnumerablesExtension
+{
+    public static void Do<T>(this IList<T> source, Action<T> action)
+    {
+        for (int i = 0; i < source.Count; i++)
+            action(source[i]);
+    }
+
+    public static void Do<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+            action(item);
+    }
+}
