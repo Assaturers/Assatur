59863d933a56e06399cffc91defe4405a1b81198
webmilio <webmilio@users.noreply.github.com>
2022-01-22 23:53:56 -0500

    Small things for entity globals.

---
+++ ModLoader/Patches/Magicka/ModLoader/Globals/IEntityGlobal.cs.patch.c
@@ -0,0 +1,9 @@
+﻿using PolygonHead;
+
+namespace Magicka.ModLoader.Globals;
+
+public interface IEntityGlobal : IModded
+{
+    public bool PreUpdate(DataChannel dataChannel, float deltaTime);
+    public bool PostUpdate(DataChannel dataChannel, float deltaTime);
+}
