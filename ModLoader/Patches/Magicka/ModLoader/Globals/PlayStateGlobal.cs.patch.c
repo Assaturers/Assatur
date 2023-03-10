da266d292a884866ba5d5178ef18e00a82a738b1
webmilio <webmilio@users.noreply.github.com>
2023-03-09 23:35:42 -0500

    An hopeful fix to compilation

--- ModLoader/Patches/Magicka/ModLoader/Globals/PlayStateGlobal.cs.patch.c
+++ ModLoader/Patches/Magicka/ModLoader/Globals/PlayStateGlobal.cs.patch.c
@@ -1,9 +1,11 @@
-﻿namespace Magicka.ModLoader.Globals;
+﻿using Magicka.GameLogic.GameStates;
+
+namespace Magicka.ModLoader.Globals;
 
 public class PlayStateGlobal : IPlayStateGlobal
 {
-    public bool PreSetDiedInLevel() => true;
-    public void PostSetDiedInLevel() { }
+    public bool PreSetDiedInLevel(PlayState state) => true;
+    public void PostSetDiedInLevel(PlayState state) { }
 
     public virtual bool InstancePer { get; }
 }
